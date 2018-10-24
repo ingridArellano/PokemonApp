cfa2ace0225887b1f809a283b81172243ca82b27
Ingrid Arellano <ingrid10arellano@gmail.com>
2018-10-24 12:01:08 -0500

    Clase Pokebola agregada

---
+++ PokemonApp/src/pokemonapp/Pokebola.java
@@ -0,0 +1,16 @@
+package pokemonapp;
+
+
+public class Pokebola {
+    
+    private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+}
