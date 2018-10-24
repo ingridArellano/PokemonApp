0624067c10d3bf3b784978ff1b72177947fd40fd
Ingrid Arellano <ingrid10arellano@gmail.com>
2018-10-24 11:40:11 -0500

    Clase pokemon agregada e instanciamiento

---
+++ PokemonApp/src/pokemonapp/Pokemon.java
@@ -0,0 +1,24 @@
+package pokemonapp;
+
+public class Pokemon {
+    
+    private String nombre;
+    private int nivel; 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+    
+}
--- PokemonApp/src/pokemonapp/PokemonApp.java
+++ PokemonApp/src/pokemonapp/PokemonApp.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pokemonapp;
 
-/**
- *
- * @author alumno
- */
 public class PokemonApp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        
+        Pokemon p = new Pokemon();
+        p.setNombre("Lunala");
+        p.setNivel(2);
+        
         System.out.println("Hola Pokemon");
+        
     }
     
 }
